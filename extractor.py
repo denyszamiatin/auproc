19532c02e7eff81c72627647c77079f7831c69c9
ValentinVerkhohliad <valentinatrix@gmail.com>
2018-03-02 16:46:53 +0200

    Created save_audio and save_tags functions

--- extractor.py
+++ extractor.py
@@ -1,6 +1,7 @@
 from glob import glob
 
 from mutagen.easyid3 import EasyID3
+import json
 
 INIT_DIR = '/Users/sergii/Downloads/mp3/*.mp3'
 
@@ -11,6 +12,16 @@ def get_filenames(dir_=INIT_DIR):
 
 def get_tags(filenames):
     return [EasyID3(filename) for filename in filenames]
+
+
+def save_audio(filename, audio):
+    with open('%s_audio.json' % filename, 'wb') as f:
+        json.dump(audio, f)
+
+
+def save_tags(filename, tags):
+    with open('%s_tags.json' % filename, 'wt') as f:
+        json.dump(tags, f)
 
 # TODO: get more clarity on objectives
 # TODO: make decision what type get_tags should return
