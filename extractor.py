5aa81e5d22151392f94fe218137c81da4945e463
denyszamiatin <denys.zamiatin@gmail.com>
2018-03-02 19:11:38 +0200

    Code-review #2

--- extractor.py
+++ extractor.py
@@ -1,27 +1,25 @@
-from glob import glob
+import glob
+import base64
+import json
 
 from mutagen.easyid3 import EasyID3
-import json
 
 INIT_DIR = '/Users/sergii/Downloads/mp3/*.mp3'
 
 
 def get_filenames(dir_=INIT_DIR):
-    return glob(dir_)
+    return glob.glob(dir_)
 
 
 def get_tags(filenames):
     return [EasyID3(filename) for filename in filenames]
 
 
-def save_audio(filename, audio):
-    with open('%s_audio.json' % filename, 'wb') as f:
-        json.dump(audio, f)
+def save_audiofile(filename, obj):
+    obj['audio'] = base64.b64encode(obj['audio'])
+    with open('%s.json' % filename, 'wt') as f:
+        json.dump(obj, f)
 
-
-def save_tags(filename, tags):
-    with open('%s_tags.json' % filename, 'wt') as f:
-        json.dump(tags, f)
 
 # TODO: get more clarity on objectives
 # TODO: make decision what type get_tags should return
