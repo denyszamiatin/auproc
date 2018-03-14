5a17d5c31fb0a668048d33b4295b715e3a5af518
annalitvin <litvin_any@ukr.net>
2018-03-15 00:43:39 +0200

    add functions convert_from_mp3 and get_volume_song for get the minimal and maximal volum level of a track

--- extractor.py
+++ extractor.py
@@ -1,10 +1,13 @@
 import glob
 import base64
 import json
+import numpy as np
+import soundfile as sf
 
 
 from mutagen.easyid3 import EasyID3
 from mutagen.mp3 import MP3
+from pydub import AudioSegment
 
 INIT_DIR = '/Users/sergii/Downloads/mp3/*.mp3'
 
@@ -25,3 +28,18 @@ def save_audiofile(filename, obj):
 
 def get_track_length(filename):
     return int(MP3(filename).info.length)
+
+
+def convert_from_mp3(filename):
+    sound = AudioSegment.from_mp3(filename)
+    name_song = filename.split(".")[0]
+    sound.export('{0}.wav'.format(name_song), format="wav")
+    return '{0}.wav'.format(name_song)
+
+
+def get_volume_song(filename):
+    rms = [np.sqrt(np.mean(block ** 2)) for block in
+           sf.blocks(filename, blocksize=1024, overlap=512)]
+
+    min_volume, max_volume = (min(rms), max(rms))
+    return min_volume, max_volume
