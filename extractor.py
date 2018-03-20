b3b5324a72a63b8af4341ca5a0381d3ed00dd0f5
annalitvin <litvin_any@ukr.net>
2018-03-20 17:30:51 +0200

    add class AudioData and correcting function get_fft

--- extractor.py
+++ extractor.py
@@ -9,35 +9,33 @@ from mutagen.mp3 import MP3
 from pydub import AudioSegment
 
 
-INIT_DIR = '/Users/sergii/Downloads/mp3/*.mp3'
+class AudioData:
 
+    INIT_DIR = '/Users/sergii/Downloads/mp3/*.mp3'
 
-def get_filenames(dir_=INIT_DIR):
-    return glob.glob(dir_)
+    def get_filenames(self, dir_=INIT_DIR):
+        return glob.glob(dir_)
 
+    def get_tags(self, filenames):
+        return [EasyID3(filename) for filename in filenames]
 
-def get_tags(filenames):
-    return [EasyID3(filename) for filename in filenames]
+    def save_audiofile(self, filename, obj):
+        obj['audio'] = base64.b64encode(obj['audio'])
+        with open('%s.json' % filename, 'wt') as f:
+            json.dump(obj, f)
 
+    def get_track_length(self, filename):
+        return int(MP3(filename).info.length)
 
-def save_audiofile(filename, obj):
-    obj['audio'] = base64.b64encode(obj['audio'])
-    with open('%s.json' % filename, 'wt') as f:
-        json.dump(obj, f)
+    def get_audiodata_from_mp3(self, filename):
+        sound = AudioSegment.from_mp3(filename)
+        return np.frombuffer(sound.raw_data, np.uint8)
 
+    def get_volume(self, stream):
+        return min(stream), max(stream)
 
-def get_track_length(filename):
-    return int(MP3(filename).info.length)
-
-
-def get_audiodata_from_mp3(filename):
-    sound = AudioSegment.from_mp3(filename)
-    return np.frombuffer(sound.raw_data, np.uint8)
-
-
-def get_volume(stream):
-    return min(stream), max(stream)
-
-
-def get_fft(filename):
-    return [np.fft.fft(block) for block in sf.blocks(filename, blocksize=1024)]
+    def get_fft(self, filename):
+        sound = AudioSegment.from_mp3(filename)
+        name_song = filename.split(".")[0]
+        sound.export('{0}.wav'.format(name_song), format="wav")
+        return [np.fft.fft(block) for block in sf.blocks('{0}.wav'.format(name_song), blocksize=1024)]
